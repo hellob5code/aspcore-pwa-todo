c12f67ba88db2aa86fc599b7228c367464707820
cecildt <cecildt@gmail.com>
2017-06-27 22:20:07 +0200

    Initial commit

---
+++ aspcore-pwa-todo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspcore_pwa_todo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "ASP.Net Core MVC Progressive Web Application TODO Example.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "No contact details available.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspcore-pwa-todo/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿(function(){
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () { console.log('Service Worker Registered'); });
+    }
+
+})();
